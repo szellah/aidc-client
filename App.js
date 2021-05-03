72ea9dc36871e84545dfe96a03e0c7e8a8e42e75
Radek <radekk0002@gmail.com>
2021-05-03 17:06:31 +0200

    Dodano RoundButtons: do ekranu 'Towar (ArticleMenu)' i 'Zarzadzanie (Managment)', oraz 'CustomRoundButton'; Dodano Buttons: 'Zatwierdz (Confirm)', 'Zapisz (Save)', 'Usun (Delete)', 'Anuluj (Cancel)'; screens: 'Towar (ArticleMenu)', 'Managment (Zarzadzanie)', 'Panel Towaru-ZapiszAnuluj (ArticlePanelSave) (poprzednio byl 'Dodawanie')', 'Panel Towaru-EdytujUsun (ArticlePanelEdit)', 'Skan lokalizacji (ScanLocation)', 'Skan towaru (ScanArticle)'; PasekNawigacyjny: 'PasekNawigacyjnyArticleMenu'; Zmieniono kolor StatusBar na czarny (w App.js)

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import Navigator from "./route/homeStack.js"
+import React from "react";
+import Navigator from "./route/homeStack.js";
+import { StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <Navigator/>
+    <>
+      {/* Zmienia kolor górnego paska (ten gdzie jest procent baterii, zasięg itd ) na czarny*/}
+      <StatusBar backgroundColor="#000"></StatusBar>
+      <Navigator />
+    </>
   );
 }
