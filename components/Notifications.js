6cfe883c479a75b4d016d75410e19be9401bc858
SebastianZellah <s.zellah10@gmail.com>
2021-05-04 13:58:41 +0200

    Ustandaryzowano Powiadomienia, dodano przyciemnienie ekranu

--- components/Notifications.js
+++ components/Notifications.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Modal, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Modal, Dimensions, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 
 /** Powiadomienie typu informacyjne oraz o błędzie w jednym komponencie. */
 
-export default function Notifications({ VisibilityHandler, message, color }) {
+export function Notification({ visibilityHandler, message, color }) {
 	return (
-		<View style={[styles.notificationBox]}>
+		<View style={[styles.notification, colors[color]['container'] ]}>
 			<TouchableOpacity
 				onPress={() => {
-					VisibilityHandler(false);
+					visibilityHandler(false);
 				}}
 			>
 				<Entypo
@@ -20,38 +20,84 @@ export default function Notifications({ VisibilityHandler, message, color }) {
 				/>
 			</TouchableOpacity>
 
-			<Text style={colors[color]}>{message}</Text>
+			<Text style={[ styles.text, colors[color]['text']]}>{message}</Text>
 		</View>
 	);
 }
 
+export function InfoNotification({visibilityHandler, message}){
+	return(
+		<Notification
+			visibilityHandler={visibilityHandler}
+			message={message}
+			color="orange"
+		/>
+	);
+}
+
+export function ErrorNotification({visibilityHandler, message}){
+	return(
+		<Notification
+			visibilityHandler={visibilityHandler}
+			message={JSON.stringify(message)}
+			color="red"
+		/>
+	);
+}
+
+export function NotificationBox({visibility, visibilityHandler, content }) {
+	/** Warunkowe otwieranie jednego z powiadomień (Błędu/ Informacji)  w obiekcie typu Modal */
+	return (
+		<Modal visible={visibility} transparent={true} animationType="fade">
+			
+			<TouchableWithoutFeedback 
+			onPress={()=>{visibilityHandler(false)}}
+			>
+
+				<View style={styles.notificationBox}>
+					{!content.error && (<InfoNotification visibilityHandler={visibilityHandler} message={content.message} />) }
+					{content.error && (<ErrorNotification visibilityHandler={visibilityHandler} message={content.message} />) }
+				</View>
+
+			</TouchableWithoutFeedback>
+		</Modal>
+	);
+}
+
 const styles = StyleSheet.create({
-	notificationBox: {
-		height: '15%',
-		backgroundColor: '#faf3ea',
+	notificationBox:{ 
+		width:"100%", 
+		height:"100%" , 
+		backgroundColor: "rgba(0,0,0,0.25)"
+	},
+	notification: {
+		height: Dimensions.get('window').height * 0.15,
 		padding: 5,
 		margin: 10,
 		textAlign: 'center',
 		borderRadius: 20,
 		marginTop: '5%',
 	},
-	textOrange: {
-		color: 'orange',
+	notificationInfo:{
+		backgroundColor: '#faf3ea',
+	},
+	notificationError:{
+		backgroundColor: '#faf300',
+	},
+	text:{
 		fontSize: 24,
 		width: '100%',
 		textAlign: 'center',
-		marginTop: '0 auto',
+	},
+	textOrange: {
+		color: 'orange',
 	},
 	textRed: {
 		color: 'tomato',
-		fontSize: 24,
-		width: '100%',
-		textAlign: 'center',
-		marginTop: '0 auto',
 	},
 	cross: {
 		textAlign: 'right',
 	},
 });
 
-const colors = { orange: styles.textOrange, red: styles.textRed };
+const colors = { orange: {text: styles.textOrange, container: styles.notificationInfo } , red: { text: styles.textRed, container: styles.notificationError } };
